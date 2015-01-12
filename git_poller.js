cfa5cffd3da259489a37e258d23c69a7e34b0a8f
Ben Arwin <ben@arwin.net>
2015-01-12 15:12:03 -0800

    Move bulk of gitNotifier code into separate file, lib/gitNotifier.js

--- git_poller.js
+++ git_poller.js
@@ -1,20 +1,11 @@
 var ASQ = require('asynquence'),
-    async = require('async'),
     config = require('config'),
-    Convert = require('ansi-to-html'),
-    convert = new Convert(),
     debug = require('debug')('git_poller'),
-    fs = require('fs'),
-    nodemailer = require('nodemailer'),
-    Git = require('git-wrapper'),
     path = require('path'),
-    _ = require('underscore'),
-    mailTransporter;
+    _ = require('underscore');
 
-// Local path where we will clone and track repos.
-var REPO_JAIL = __dirname + '/var',
-    EMAIL_TO = config.get('email.to'),
-    EMAIL_FROM = config.get('email.from');
+var GitNotifier = require('./lib/gitNotifier'),
+    gitNotifier = new GitNotifier();
 
 
 _.each(config.get('repoList'), function(repo) {
@@ -24,7 +15,7 @@ _.each(config.get('repoList'), function(repo) {
     ASQ(repoUrl).
         then(function(done, repoUrl) {
             // Check that repo is checked out in our 'jail'
-            cloneRepoIfNotExists(repoUrl, function(err) {
+            gitNotifier.cloneRepoIfNotExists(repoUrl, function(err) {
                 if (err) { done.fail(err); }
                 else {
                     done();
@@ -32,7 +23,7 @@ _.each(config.get('repoList'), function(repo) {
             });
         })
         .then(function(done) {
-            checkForNewCommits(repoUrl, function(err, ansiLogAndDiff, remoteSha1) {
+            gitNotifier.checkForNewCommits(repoUrl, function(err, ansiLogAndDiff, remoteSha1) {
                 if (err) { done.fail(err); }
                 else {
                     if (ansiLogAndDiff) {
@@ -46,7 +37,7 @@ _.each(config.get('repoList'), function(repo) {
             });
         })
         .then(function(done, ansiLogAndDiff, remoteSha1) {
-            sendEmailNotification(repoUrl, ansiLogAndDiff, remoteSha1, function(err, info) {
+            gitNotifier.sendEmailNotification(repoUrl, ansiLogAndDiff, remoteSha1, function(err, info) {
                 if (err) {
                     console.error("Failed to send email:", err);
                     done.fail(err);
@@ -62,162 +53,3 @@ _.each(config.get('repoList'), function(repo) {
             throw err;
         });
 });
-
-
-/**
- * Clone repo if it's not already cloned.
- *
- * New repo clone will be a shallow bare clone.
- *
- * @param {string} repoUrl
- * @param {function} callback
- */
-function cloneRepoIfNotExists(repoUrl, callback) {
-    var localRepoPath = localPath(repoUrl);
-
-    fs.exists(localRepoPath, function(exists) {
-        if (!exists) {
-            debug("Repo " + repoUrl + " does not exist locally yet. Cloning to " + localRepoPath);
-            new Git().exec('clone', { bare: true, depth: 1 }, [ repoUrl, localRepoPath ], function(err, msg) {
-                if (err) {
-                    console.error("Failed to clone repo: " + repoUrl + ":", err);
-                }
-                else {
-                    debug("Successfully cloned repo: %s", localRepoPath, msg);
-                }
-                doCallback(callback, [err]);
-            });
-        }
-        else {
-            doCallback(callback);
-        }
-    });
-}
-
-/**
- * Fetches new commits from origin, and returns the diff.
- *
- * @param {string} repoUrl
- * @param {function} callback (err, diff)
- */
-function checkForNewCommits(repoUrl, callback) {
-
-    var localRepoPath = localPath(repoUrl),
-        git = new Git({ 'git-dir': localRepoPath });
-
-    ASQ()
-        .then(function(done) {
-            // get latest LOCAL sha1
-            git.exec('rev-parse', {}, ['HEAD'], function(err, localSha1) {
-                if (err) { done.fail(err); }
-                else {
-                    done(localSha1.trim());
-                }
-            });
-        })
-        .then(function(done, localSha1) {
-            // get latest REMOTE sha1
-            git.exec('ls-remote', {}, [ 'origin', 'HEAD' ], function(err, remoteSha1Line) {
-                if (err) { done.fail(err); }
-                else {
-                    var remoteSha1 = remoteSha1Line.split(/\s/)[0];
-                    done(localSha1, remoteSha1);
-                }
-            });
-        })
-        .then(function(done, localSha1, remoteSha1) {
-            if (localSha1 === remoteSha1) {
-                // no new commits
-                debug("No new commits found for " + path.basename(repoUrl));
-                done();
-            }
-            else {
-                // We have new commits!
-                debug("There are new commits!");
-                // Fetch, log, and diff
-                git.exec('fetch', {}, [ 'origin', 'master:master' ], function(err) {
-                    if (err) { done.fail(err); }
-                    else {
-                        // Git 'log' and 'diff' take [mostly] the same flags/args.
-                        // So define here for convenience.
-                        var gitOpts = { color: true, paginate: false},
-                            gitArgs = [ localSha1+".."+remoteSha1 ];
-
-                        async.parallel([
-                            function(cb) {
-                                git.exec('log', _.extend(_.clone(gitOpts), { stat: true }), gitArgs, cb);
-                            },
-                            function(cb) {
-                                git.exec('diff', gitOpts, gitArgs, cb);
-                            }
-                        ],
-                        function(err, results) {
-                            var ansiLog = results[0],
-                                ansiDiff = results[1];
-
-                            if (err) { done.fail(err); }
-                            else {
-                                done(ansiLog + "\n\n" + ansiDiff, remoteSha1);
-                            }
-                        });
-                    }
-                });
-            }
-        })
-        .val(function(ansiLogAndDiff, remoteSha1) {
-            doCallback(callback, [null, ansiLogAndDiff, remoteSha1]);
-        })
-        .or(function(err) {
-            doCallback(callback, [err]);
-        });
-}
-
-
-/**
- * Fire off the email containing our git diff
- *
- * @param {string} repoUrl
- * @param {string} ansiLogAndDiff
- * @param {string} remoteSha1
- * @param {function} callback
- */
-function sendEmailNotification(repoUrl, ansiLogAndDiff, remoteSha1, callback) {
-    // ansi-to-html doesn't include line breaks, so we must add those ourselves.
-    var htmlBody = convert.toHtml(ansiLogAndDiff).replace(/\n/g, '\n<br>');
-
-    // Send email!
-    debug("Sending email from=%s to=%s", EMAIL_FROM, EMAIL_TO);
-    var mailOptions = {
-        from: EMAIL_FROM,
-        to: EMAIL_TO,
-        subject: "[Git] new commits in " + path.basename(repoUrl) + " " + remoteSha1.substring(0,8),
-        html: htmlBody
-    };
-
-    if (mailTransporter === undefined) {
-        // Initialize mailTransporter on first use.
-
-        // Clone smtpOptions get a non-read-only object.
-        // (Nodemailer needs to write to the object).
-        mailTransporter = nodemailer.createTransport(_.clone(config.get('email.smtpOptions')));
-    }
-
-    mailTransporter.sendMail(mailOptions, callback);
-}
-
-
-function localPath(repoUrl) {
-    return REPO_JAIL + '/' + path.basename(repoUrl);
-}
-
-/**
- * Wrapper for conditionally calling a callback.
- *
- * @param {function} cb
- * @param {Array} args Optional arguments to the callback
- */
-function doCallback(cb, args) {
-    if (cb) {
-        cb.apply(null, args);
-    }
-}
