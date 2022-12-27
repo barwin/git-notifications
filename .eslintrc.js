eb2405be42bde4f40751f5ca148ca64806fbdc2e
Ben Arwin <github@arwin.net>
2022-12-27 10:18:19 -0500

    chore: Cleanup eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
     env: {
-        browser: true,
-        commonjs: true,
         es2020: true,
     },
     extends: [
         'airbnb-base',
     ],
-    parserOptions: {
-        ecmaVersion: 12,
-    },
     rules: {
         'arrow-body-style': [0],
         'arrow-parens': [0],
